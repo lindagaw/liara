5beb9c336a86518171c6d5399870ebc05bdb9357
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-24 16:45:08 -0500

    IP

--- main.py
+++ main.py
@@ -31,10 +31,9 @@ def train_step(images):
 
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
         print(images.shape)
-        print(generated_images.shape)
 
         generated_images = generator(noise, training=True)
-
+        print(generated_images.shape)
         real_output = discriminator(images, training=True)
         fake_output = discriminator(generated_images, training=True)
 
