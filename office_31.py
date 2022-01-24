c0b2c45c2ca4c531c58e06066bff9c446becf69c
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-24 17:02:50 -0500

    WIP

--- office_31.py
+++ office_31.py
@@ -12,7 +12,7 @@ def office_31_subset(dataset):
 
         category_index = os.listdir(dataset_path).index(category)
         for image_name in os.listdir(category_path):
-            standard_size = (100, 100)
+            standard_size = (56, 56)
             image = np.asarray(Image.open(os.path.join('office-31', dataset, 'images', category, image_name)).resize(standard_size))
             xs.append(image)
             ys.append(category_index)
