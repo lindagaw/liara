d78145f6eb9d55cec31081c1b8cae109f533c390
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-24 15:48:17 -0500

    WIP

--- main.py
+++ main.py
@@ -1,32 +1,22 @@
 import os
 import numpy as np
+import tensorflow as tf
 
 from office_31 import office_31_subset
+from dcgan_generator import make_generator_model
+from dcgan_discriminator import make_discriminator_model
 
 office_31_subset('amazon')
 office_31_subset('dslr')
 office_31_subset('webcam')
 
-def make_generator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
 
-    model.add(layers.Reshape((7, 7, 256)))
-    assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
+generator = make_generator_model()
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-    assert model.output_shape == (None, 7, 7, 128)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+noise = tf.random.normal([1, 100])
+generated_image = generator(noise, training=False)
 
-    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 14, 14, 64)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-    assert model.output_shape == (None, 28, 28, 1)
-
-    return model
+discriminator = make_discriminator_model()
+decision = discriminator(generated_image)
+print (decision)
