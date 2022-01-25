5a446d138acbe5ccbb4da8be3933b5237d0e0c33
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-25 03:01:54 -0500

    WIP

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from tensorflow.keras import layers
+
 from office_31 import office_31_subset
 from dcgan_generator import make_generator_model, generator_loss
 from dcgan_discriminator import make_discriminator_model, discriminator_loss
@@ -7,10 +9,12 @@ amazon_xs, amazon_ys = office_31_subset('amazon')
 dslr_xs, dlsr_ys = office_31_subset('dslr')
 webcam_xs, webcam_ys = office_31_subset('webcam')
 
+
 resnet_model = tf.keras.applications.resnet50.ResNet50(
     include_top=True, weights='imagenet', input_tensor=None,
     input_shape=(224, 224, 3), pooling=None, classes=1000
 )
+resnet_model.add(layers.Dense(31))
 
 results = resnet_model.predict(amazon_xs)
 
