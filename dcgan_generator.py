2b4bf13e604307a974b8a4cc96de8b5bbaaf9db6
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-25 13:37:29 -0500

    WIP

--- dcgan_generator.py
+++ dcgan_generator.py
@@ -14,13 +14,13 @@ cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 def make_generator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    #model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    #model.add(layers.BatchNormalization())
+    #model.add(layers.LeakyReLU())
 
-    model.add(layers.Reshape((7, 7, 256)))
+    #model.add(layers.Reshape((7, 7, 256)))
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), input_shape=[56, 56, 3], padding='same', use_bias=False))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
