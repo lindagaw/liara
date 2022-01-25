a39bf124bad93130fa05b54513645d9a87aed9ab
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-25 02:53:53 -0500

    WIP

--- main.py
+++ main.py
@@ -11,3 +11,5 @@ resnet_model = tf.keras.applications.resnet50.ResNet50(
     include_top=True, weights='imagenet', input_tensor=None,
     input_shape=(224, 224, 3), pooling=None, classes=1000
 )
+
+results = resnet_model.predict(amazon)
