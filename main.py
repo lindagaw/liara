16c6466a29f3bcab78e28b945479420dd25e40fc
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-25 03:44:38 -0500

    WIP

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ webcam_xs, webcam_ys = office_31_subset('webcam')
 
 
 resnet_model = tf.keras.applications.resnet50.ResNet50(
-    include_top=False, weights='imagenet', input_tensor=None,
-    input_shape=(224, 224, 3), pooling=None, classes=31
+    include_top=True, weights='imagenet', input_tensor=None,
+    input_shape=(32, 32, 3), pooling=None, classes=31
 )
 
 
