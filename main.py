0858c68e4fd253cca272bac20c6fabb67d4f0ff4
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-29 01:15:16 -0500

    WIP

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ ngpu = 4
 src = "amazon"
 tgt = "dslr"
 
-src_obj = tgt_obj = "projector"
+src_obj = tgt_obj = "bookcase"
 
 print('available objs are {}'.format(os.listdir("office-31//amazon//images//")))
 
