f15babdb2400b5e50a2357062d2bf47444f706d4
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-29 12:17:00 -0500

    WIP

--- generator.py
+++ generator.py
@@ -76,6 +76,10 @@ class Generator(nn.Module):
             nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
+
+            nn.ConvTranspose2d( ngf * 2, ngf, 1, 1, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
             # state size. (ngf) x 32 x 32
             nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
