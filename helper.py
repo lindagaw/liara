ab888768bc9135cbe6524a68e102f87439bf0991
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-28 23:37:55 -0500

    WIP

--- helper.py
+++ helper.py
@@ -44,6 +44,7 @@ def save_individual_images(path_to_save_to, tensor):
 
     for i in range(0, len(tensor)):
 
-        data = Image.fromarray(tensor[i].numpy().astype(np.uint8))
+        data = Image.fromarray(tensor[i].numpy() * 255)
+        data = data.astype(np.uint8)
         path = path_to_save_to + str(i) + '.png'
         data.save(path)
