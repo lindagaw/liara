618c16d6e3d20320c426568b28afd9f45cc768fb
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-29 23:00:00 -0500

    wip

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ src_tgt_train_loader = torch.utils.data.DataLoader(dataset_src_tgt_train, batch_
 src_tgt_fake_train_loader = torch.utils.data.DataLoader(dataset_src_tgt_fake_train, batch_size=batch_size, shuffle=True)
 
 classifier = f.cuda()
-classifier = train(classifier, dataloader_tgt_train)
+classifier = train(classifier, dataloader_fake)
 
 acc = eval(classifier, dataloader_src_test)
 acc = eval(classifier, dataloader_tgt_test)
