a6845be1bb7959f8dff3ff4a01dfcd8a230bdbcd
Ashley Gao <gaoye0718@gmail.com>
2022-02-01 12:49:12 -0500

    WIP

--- demo.py
+++ demo.py
@@ -47,13 +47,11 @@ nc = 3
 nz = 100
 ngf = 64
 ndf = 64
-num_epochs = 30
+num_epochs = 300
 lr = 0.0001
 beta1 = 0.5
 ngpu = 4
 
-src = "amazon"
-tgt = "dslr"
 
 src_obj = "male"
 tgt_obj = "female"
