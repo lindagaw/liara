2573db7cfa52399676de6abad5c0aa2f56a56f4b
Ashley Gao <gaoye0718@gmail.com>
2022-02-01 16:22:33 -0500

    WIP

--- create_fake_dataset.py
+++ create_fake_dataset.py
@@ -133,9 +133,11 @@ for obj in os.listdir("datasets//office-31//amazon//images//"):
         fake_label = 0.
 
         # Setup Adam optimizers for both G and D
-        optimizerD = optim.SGD(netD.parameters(), lr=lr, betas=(beta1, 0.999))
-        optimizerD_tgt = optim.SGD(netD_tgt.parameters(), lr=lr, betas=(beta1, 0.999))
+        #optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+        #optimizerD_tgt = optim.Adam(netD_tgt.parameters(), lr=lr, betas=(beta1, 0.999))
         optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+        optimizerD = optim.SGD(netD.parameters(), lr=lr)
+        optimizerD_tgt = optim.SGD(netD_tgt.parameters(), lr=lr)
 
         # Training Loop
 
