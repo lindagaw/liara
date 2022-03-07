34b93946366b73870583a2b7539a35322d09abc2
Ashley Gao <gaoye0718@gmail.com>
2022-03-07 18:04:48 -0500

    WIP

--- main_cifar_stl.py
+++ main_cifar_stl.py
@@ -112,7 +112,7 @@ dataloader_fake = torch.utils.data.DataLoader(dataset_fake, batch_size=batch_siz
                                          shuffle=True)
 
 dataset_src_tgt_train = ConcatDataset((dataset_src_train, dataset_tgt_train))
-dataset_src_tgt_fake_train = ConcatDataset((dataset_src_train, dataset_tgt_train, dataset_fake))
+dataset_src_tgt_fake_train = ConcatDataset((dataset_src_train, dataset_fake))
 
 src_tgt_train_loader = torch.utils.data.DataLoader(dataset_src_tgt_train, batch_size=batch_size, shuffle=True)
 src_tgt_fake_train_loader = torch.utils.data.DataLoader(dataset_src_tgt_fake_train, batch_size=batch_size, shuffle=True)
