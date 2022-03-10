c08563aaa4a6a212a187e279a2de2c6b1117dc33
Ashley Gao <gaoye0718@gmail.com>
2022-03-09 21:06:29 -0500

    wip

--- generate_fake_mnist_usps.py
+++ generate_fake_mnist_usps.py
@@ -80,8 +80,6 @@ dataset_tgt = datasets.USPS(root='./data',
 
 dataset_tgt.targets = torch.tensor(dataset_tgt.targets)
 idx = get_same_index(dataset_tgt.targets, category)
-
-print(idx)
 dataset_tgt.targets= dataset_tgt.targets[idx]
 dataset_tgt.data = dataset_tgt.data[idx]
 
