ba3ee90951a26e8cece8a445aa8e89ab1e62f22a
Ashley Gao <gaoye0718@gmail.com>
2022-03-09 19:40:35 -0500

    wip

--- misc/helper.py
+++ misc/helper.py
@@ -132,11 +132,13 @@ def get_particular_class(dataset, category, order):
 
     for target, sample in zip(targets, data):
         if target == category:
-            new_targets.append(target.numpy())
+            new_targets.append(target)
             if order == 'svhn':
                 new_data.append(sample.transpose(2,1,0))
             else:
-                new_data.append(sample.numpy())
+                new_data.append(sample)
+
+    print(new_targets)
 
     return new_data, new_targets
 
