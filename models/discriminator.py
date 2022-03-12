a4edde3b19120e772882d661415fe7c277fc7c97
Ashley Gao <gaoye0718@gmail.com>
2022-03-12 05:00:37 -0500

    wip

--- models/discriminator.py
+++ models/discriminator.py
@@ -56,7 +56,7 @@ ngpu = 4
 
 
 class Discriminator(nn.Module):
-    def __init__(self, ngpu=4):
+    def __init__(self, ngpu):
         super(Discriminator, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
@@ -84,49 +84,29 @@ class Discriminator(nn.Module):
         return self.main(input)
 '''
 class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+            # input is (nc) x 128 x 128
+            nn.Conv2d(nc,ndf,4,2,1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 64 x 64
+            nn.Conv2d(ndf,ndf*2,4,2,1, bias=False),
+            nn.InstanceNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 32 x 32
+            nn.Conv2d(ndf*2, ndf * 4, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 16 x 16
+            nn.Conv2d(ndf*4,ndf*8,4,1,1),
+            nn.InstanceNorm2d(ndf*8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 15 x 15
+            nn.Conv2d(ndf*8,1,4,1,1)
+            # state size. 1 x 14 x 14
+        )
 
-    def __init__(self, in_channels, out_channels, stride=1):
-        super(ResBlockDiscriminator, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, 1, padding=1)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, 1, padding=1)
-        nn.init.xavier_uniform(self.conv1.weight.data, 1.)
-        nn.init.xavier_uniform(self.conv2.weight.data, 1.)
-
-        if stride == 1:
-            self.model = nn.Sequential(
-                nn.ReLU(),
-                SpectralNorm(self.conv1),
-                nn.ReLU(),
-                SpectralNorm(self.conv2)
-                )
-        else:
-            self.model = nn.Sequential(
-                nn.ReLU(),
-                SpectralNorm(self.conv1),
-                nn.ReLU(),
-                SpectralNorm(self.conv2),
-                nn.AvgPool2d(2, stride=stride, padding=0)
-                )
-        self.bypass = nn.Sequential()
-        if stride != 1:
-
-            self.bypass_conv = nn.Conv2d(in_channels,out_channels, 1, 1, padding=0)
-            nn.init.xavier_uniform(self.bypass_conv.weight.data, np.sqrt(2))
-
-            self.bypass = nn.Sequential(
-                SpectralNorm(self.bypass_conv),
-                nn.AvgPool2d(2, stride=stride, padding=0)
-            )
-            # if in_channels == out_channels:
-            #     self.bypass = nn.AvgPool2d(2, stride=stride, padding=0)
-            # else:
-            #     self.bypass = nn.Sequential(
-            #         SpectralNorm(nn.Conv2d(in_channels,out_channels, 1, 1, padding=0)),
-            #         nn.AvgPool2d(2, stride=stride, padding=0)
-            #     )
-
-
-    def forward(self, x):
-        return self.model(x) + self.bypass(x)
+    def forward(self, input):
+        return self.main(input)
 '''
