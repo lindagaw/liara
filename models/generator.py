9cfcfd75d496ade3eee000710351282ce3932404
Ashley Gao <gaoye0718@gmail.com>
2022-03-12 05:11:53 -0500

    wip

--- models/generator.py
+++ models/generator.py
@@ -54,7 +54,7 @@ beta1 = 0.5
 # Number of GPUs available. Use 0 for CPU mode.
 ngpu = 4
 
-
+'''
 class Generator(nn.Module):
     def __init__(self, ngpu=4):
         super(Generator, self).__init__()
@@ -87,32 +87,62 @@ class Generator(nn.Module):
         return self.main(input)
 '''
 
-norm_layer = nn.InstanceNorm2d
-class ResBlock(nn.Module):
-    def __init__(self, f):
-        super(ResBlock, self).__init__()
-        self.conv = nn.Sequential(nn.Conv2d(f, f, 3, 1, padding=1), norm_layer(f), nn.ReLU(),
-                                  nn.Conv2d(f, f, 3, 1, padding=1))
-        self.norm = norm_layer(f)
-    def forward(self, x):
-        return F.relu(self.norm(self.conv(x)+x))
+class ResnetGenerator(nn.Module):
+    """Resnet-based generator that consists of Resnet blocks between a few downsampling/upsampling operations.
 
-class Generator(nn.Module):
-    def __init__(self, f=64, blocks=6):
-        super(Generator, self).__init__()
-        layers = [nn.ReflectionPad2d(3),
-                  nn.Conv2d(  3,   f, 7, 1, padding=1), norm_layer(  f), nn.ReLU(True),
-                  nn.Conv2d(  f, 2*f, 3, 2, padding=1), norm_layer(2*f), nn.ReLU(True),
-                  nn.Conv2d(2*f, 4*f, 3, 2, padding=1), norm_layer(4*f), nn.ReLU(True)]
-        for i in range(int(blocks)):
-            layers.append(ResBlock(4*f))
-        layers.extend([
-                nn.ConvTranspose2d(4*f, 4*2*f, 3, 1, padding=1), nn.PixelShuffle(2), norm_layer(2*f), nn.ReLU(True),
-                nn.ConvTranspose2d(2*f,   4*f, 3, 1, padding=1), nn.PixelShuffle(2), norm_layer(  f), nn.ReLU(True),
-                nn.ReflectionPad2d(3), nn.Conv2d(f, 3, 7, 1, padding=1),
-                nn.Tanh()])
-        self.conv = nn.Sequential(*layers)
+    We adapt Torch code and idea from Justin Johnson's neural style transfer project(https://github.com/jcjohnson/fast-neural-style)
+    """
 
-    def forward(self, x):
-        return self.conv(x)
-'''
+    def __init__(self, input_nc=nc, output_nc=nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=6, padding_type='reflect'):
+        """Construct a Resnet-based generator
+
+        Parameters:
+            input_nc (int)      -- the number of channels in input images
+            output_nc (int)     -- the number of channels in output images
+            ngf (int)           -- the number of filters in the last conv layer
+            norm_layer          -- normalization layer
+            use_dropout (bool)  -- if use dropout layers
+            n_blocks (int)      -- the number of ResNet blocks
+            padding_type (str)  -- the name of padding layer in conv layers: reflect | replicate | zero
+        """
+        assert(n_blocks >= 0)
+        super(ResnetGenerator, self).__init__()
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+
+        model = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias),
+                 norm_layer(ngf),
+                 nn.ReLU(True)]
+
+        n_downsampling = 2
+        for i in range(n_downsampling):  # add downsampling layers
+            mult = 2 ** i
+            model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=use_bias),
+                      norm_layer(ngf * mult * 2),
+                      nn.ReLU(True)]
+
+        mult = 2 ** n_downsampling
+        for i in range(n_blocks):       # add ResNet blocks
+
+            model += [ResnetBlock(ngf * mult, padding_type=padding_type, norm_layer=norm_layer, use_dropout=use_dropout, use_bias=use_bias)]
+
+        for i in range(n_downsampling):  # add upsampling layers
+            mult = 2 ** (n_downsampling - i)
+            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+                                         kernel_size=3, stride=2,
+                                         padding=1, output_padding=1,
+                                         bias=use_bias),
+                      norm_layer(int(ngf * mult / 2)),
+                      nn.ReLU(True)]
+        model += [nn.ReflectionPad2d(3)]
+        model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+        model += [nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, input):
+        """Standard forward"""
+        return self.model(input)
