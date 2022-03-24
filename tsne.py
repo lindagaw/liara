d290b4655c3fb2edbbf33b85332c7297db1ba6e1
Ashley Gao <gaoye0718@gmail.com>
2022-03-24 15:23:53 -0400

    wip

--- tsne.py
+++ tsne.py
@@ -143,13 +143,13 @@ import seaborn as sns
 n_components = 2
 tsne = TSNE(n_components)
 tsne_result = tsne.fit_transform(X)
-tsne_result.shape
+print(tsne_result.shape)
 # (1000, 2)
 # Two dimensions for each of our images
 
 # Plot the result of our TSNE with the label color coded
 # A lot of the stuff here is about making the plot look pretty and not TSNE
-tsne_result_df = pd.DataFrame({'tsne_1': tsne_result[:,0], 'tsne_2': tsne_result[:,1], 'label': y})
+#tsne_result_df = pd.DataFrame({'tsne_1': tsne_result[:,0], 'tsne_2': tsne_result[:,1], 'label': y})
 fig, ax = plt.subplots(1)
 sns.scatterplot(x='tsne_1', y='tsne_2', hue='label', data=tsne_result_df, ax=ax,s=120)
 lim = (tsne_result.min()-5, tsne_result.max()+5)
