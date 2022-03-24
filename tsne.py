d890678e059d3ec7dc3d4ea41de38210be493377
Ashley Gao <gaoye0718@gmail.com>
2022-03-24 15:34:59 -0400

    wip

--- tsne.py
+++ tsne.py
@@ -138,20 +138,20 @@ y = np.vstack((red, green))
 from sklearn.manifold import TSNE
 import pandas as pd
 import seaborn as sns
+
 # We want to get TSNE embedding with 2 dimensions
 n_components = 2
 tsne = TSNE(n_components)
-Y = tsne.fit_transform(X)
-print(Y.shape)
+tsne_result = tsne.fit_transform(X)
+print(tsne_result.shape)
 # (1000, 2)
 # Two dimensions for each of our images
 
 # Plot the result of our TSNE with the label color coded
 # A lot of the stuff here is about making the plot look pretty and not TSNE
-#tsne_result_df = pd.DataFrame({'tsne_1': tsne_result[:,0], 'tsne_2': tsne_result[:,1], 'label': y})
+tsne_result_df = pd.DataFrame({'tsne_1': tsne_result[:,0], 'tsne_2': tsne_result[:,1], 'label': y})
 fig, ax = plt.subplots(1)
-#sns.scatterplot(x='tsne_1', y='tsne_2', hue='label', data=tsne_result_df, ax=ax,s=120)
-ax.scatter(Y[:, 0], Y[:, 1], c=['r'], cmap=plt.cm.Spectral)
+sns.scatterplot(x='tsne_1', y='tsne_2', hue='label', data=tsne_result_df, ax=ax,s=120)
 lim = (tsne_result.min()-5, tsne_result.max()+5)
 ax.set_xlim(lim)
 ax.set_ylim(lim)
