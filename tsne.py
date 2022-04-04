cd7ff63756260ef1ea4704a3d9ca16235ec08e85
Ashley Gao <gaoye0718@gmail.com>
2022-04-04 15:07:15 -0400

    WIP

--- tsne.py
+++ tsne.py
@@ -59,7 +59,7 @@ transform=transforms.Compose([
     transforms.Resize(image_size),
     transforms.CenterCrop(image_size),
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
 ])
 # Batch size during training
 
