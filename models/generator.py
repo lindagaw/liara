2110e7680f54fbf24ee49093005fb63eaea37651
Ashley Gao <gaoye0718@gmail.com>
2022-04-04 16:43:23 -0400

    WIP

--- models/generator.py
+++ models/generator.py
@@ -91,29 +91,27 @@ class Generator_Reverse(nn.Module):
         super(Generator_Reverse, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
-            #nn.Tanh()
-            # state size. (nc) x 64 x 64
-
-            # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf),
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d( 3, ngf * 16, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 16),
             nn.ReLU(True),
-
-            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2),
-            nn.ReLU(True),
-
+            # state size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(ngf * 16, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
 
-            nn.ConvTranspose2d( nz, ngf * 16, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 16),
+            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
         )
 
     def forward(self, input):
