3f38ab343c5b583eb6417209426d0d717e5739f9
Ashley Gao <gaoye0718@gmail.com>
2022-04-04 16:02:35 -0400

    WIP

--- misc/normalize.py
+++ misc/normalize.py
@@ -16,27 +16,19 @@ train_transform = transforms.Compose([transforms.ToTensor()])
 
 # cifar10
 train_set = datasets.CIFAR10(root='../data/', train=True, download=True, transform=train_transform)
-print(train_set.train_data.shape)
-print(train_set.train_data.mean(axis=(0,1,2))/255)
-print(train_set.train_data.std(axis=(0,1,2))/255)
+print(train_set.data.shape)
+print(train_set.data.mean(axis=(0,1,2))/255)
+print(train_set.data.std(axis=(0,1,2))/255)
 # (50000, 32, 32, 3)
 # [0.49139968  0.48215841  0.44653091]
 # [0.24703223  0.24348513  0.26158784]
 
-# cifar100
-train_set = datasets.CIFAR100(root='../data/', train=True, download=True, transform=train_transform)
-print(train_set.train_data.shape)
-print(train_set.train_data.mean(axis=(0,1,2))/255)
-print(train_set.train_data.std(axis=(0,1,2))/255)
-# (50000, 32, 32, 3)
-# [0.50707516  0.48654887  0.44091784]
-# [0.26733429  0.25643846  0.27615047]
 
 # mnist
 train_set = datasets.STL10(root='../data/', train=True, download=True, transform=train_transform)
-print(train_set.train_data.shape)
-print(train_set.train_data.mean(axis=(0,1,2))/255)
-print(train_set.train_data.std(axis=(0,1,2))/255)
+print(train_set.data.shape)
+print(train_set.data.mean(axis=(0,1,2))/255)
+print(train_set.data.std(axis=(0,1,2))/255)
 # [60000, 28, 28]
 # 0.1306604762738429
 # 0.30810780717887876
