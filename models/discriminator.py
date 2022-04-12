39ee08875a0bff87ca1a3a23c920c1b853155899
Ashley Gao <gaoye0718@gmail.com>
2022-04-12 17:08:07 -0400

    WIP

--- models/discriminator.py
+++ models/discriminator.py
@@ -65,15 +65,15 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.LayerNorm2d(ndf * 2),
+            nn.LayerNorm(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*2) x 16 x 16
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.LayerNorm2d(ndf * 4),
+            nn.LayerNorm(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*4) x 8 x 8
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.LayerNorm2d(ndf * 8),
+            nn.LayerNorm(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
             nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
