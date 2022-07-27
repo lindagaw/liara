a68a56c5b51e41698900a6cc1d039c6c7799ab50
Ashley Gao <gaoye0718@gmail.com>
2022-07-27 13:39:29 -0400

    WIP

--- main_office_31.py
+++ main_office_31.py
@@ -75,7 +75,7 @@ train_portion_set_amazon, _ = torch.utils.data.random_split(train_set_amazon, [i
 train_portion_set_dslr, _ = torch.utils.data.random_split(train_set_dslr, [int(len(train_set_dslr)*0.2), len(train_set_dslr)-int(len(train_set_dslr)*0.2)])
 train_portion_set_webcam, _ = torch.utils.data.random_split(train_set_webcam, [int(len(train_set_webcam)*0.2), len(train_set_webcam)-int(len(train_set_webcam)*0.2)])
 
-train_amazon_webcam = ConcatDataset((train_set_amazon, train_portion_set_webcam))
+train_amazon_webcam = ConcatDataset((train_portion_set_amazon, train_set_webcam))
 train_amazon_dslr = ConcatDataset((train_set_amazon, train_set_dslr))
 train_webcam_dslr = ConcatDataset((train_set_webcam, train_set_dslr))
 
