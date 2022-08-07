70cf8a96f525447f4555be0902ed8ae97a07e98f
Ashley Gao <gaoye0718@gmail.com>
2022-08-07 17:16:15 -0400

    wip

--- main_office_31.py
+++ main_office_31.py
@@ -76,7 +76,7 @@ train_set_webcam, test_set_webcam = torch.utils.data.random_split(dataset_webcam
 #train_portion_set_webcam, _ = torch.utils.data.random_split(train_set_webcam, [31*3, len(train_set_webcam)-31*3])
 
 train_amazon_webcam = ConcatDataset((train_set_amazon, train_set_webcam))
-train_amazon_dslr = ConcatDataset((train_portion_set_amazon, train_set_dslr))
+train_amazon_dslr = ConcatDataset((train_set_amazon, train_set_dslr))
 train_webcam_dslr = ConcatDataset((train_set_webcam, train_set_dslr))
 
 #train_amazon_dslr = ConcatDataset((train_set_amazon,dataset_amazon_dslr, train_set_dslr))
