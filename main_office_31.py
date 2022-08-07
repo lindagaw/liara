60913da02ae90b097b993d90fdebff8c50ca6030
Ashley Gao <gaoye0718@gmail.com>
2022-08-07 18:02:24 -0400

    WIP

--- main_office_31.py
+++ main_office_31.py
@@ -113,8 +113,8 @@ f.fc = nn.Linear(2048, 31)
 classifier = f.cuda()
 
 #classifier = train(classifier, dataloader_train_amazon_webcam)
-classifier = train(classifier, dataloader_train_amazon_dslr)
-#classifier = train(classifier, dataloader_train_webcam_dslr)
+#classifier = train(classifier, dataloader_train_amazon_dslr)
+classifier = train(classifier, dataloader_train_webcam_dslr)
 
 #classifier = train(classifier, dataloader_train_amazon)
 
